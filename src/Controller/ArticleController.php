3469cd5fa6134dcc30e424c976aaae4564bbcea1
KalaniM <kalakalanicontact@gmail.com>
2019-04-04 17:31:51 +0200

    Building up!

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,12 +9,26 @@
     namespace App\Controller;
     
     
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
     use Symfony\Component\HttpFoundation\Response;
 
     class ArticleController
     {
+        /**
+         * @Route("/")
+         */
         public function homepage()
         {
             return new Response('My first page !');
+        }
+    
+        /**
+         * @Route("/news/{slug}")
+         */
+        public function show($slug)
+        {
+            return new Response(sprintf(
+                'Future page to show article : %s', $slug
+            ));
         }
     }
