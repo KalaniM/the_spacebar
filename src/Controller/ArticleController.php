88f1a6faf9aacaf83da790f043bd6a923b1f1955
KalaniM <kalakalanicontact@gmail.com>
2019-04-04 17:11:34 +0200

    It works !

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,7 +9,12 @@
     namespace App\Controller;
     
     
+    use Symfony\Component\HttpFoundation\Response;
+
     class ArticleController
     {
-        
+        public function homepage()
+        {
+            return new Response('My first page !');
+        }
     }
