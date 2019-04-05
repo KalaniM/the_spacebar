65245930518b7bc17af2136cced92db9a7b331ca
KalaniM <kalakalanicontact@gmail.com>
2019-04-05 16:16:43 +0200

    log

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,6 +9,7 @@
     namespace App\Controller;
     
     
+    use Psr\Log\LoggerInterface;
     use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\JsonResponse;
@@ -45,9 +46,12 @@
         /**
          * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
          */
-        public function toggleArticleHeart($slug)
+        public function toggleArticleHeart($slug, LoggerInterface $logger)
         {
             // TODO - actually heart/unheart the article
+            
+            $logger->info('Article is being hearted');
+            
             return new JsonResponse(['hearts' => rand(5, 100)]);
         }
         
